314cae60fff3dd12db026f1c09a6ca1270397837
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2021-03-05 21:08:51 -0500

    tmpl initial commit

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 python3Packages.buildPythonApplication rec {
-  pname = "&&NAME&&";
+  pname = "room";
   version = "&&VERSION";
   src = ./.;
   propagatedBuildInputs = [];
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-NAME = "&&NAME&&"
+NAME = "room"
 
 setup(
     name=NAME,
-    version="&&VERSION&&",
+    version="0.0.0",
     py_modules=[NAME],
     entry_points={"console_scripts": [f"{{NAME}}={{NAME}}:main"]},
 )
